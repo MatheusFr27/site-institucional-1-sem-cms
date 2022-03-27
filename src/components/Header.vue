11aab7157c268515ebfddbfd7097e1126a057e44
Matheus Eduardo França <matheusefranca1727@gmail.com>
2022-03-26 22:08:00 -0300

    Implementando component header

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app-bar height="95em" app color="blue-grey darken-3" dark>
+    <v-img
+      alt="Vuetify Logo"
+      class="shrink ml-4"
+      contain
+      src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+      transition="scale-transition"
+      width="40"
+    />
+
+    <v-spacer></v-spacer>
+
+    <div class="d-none d-sm-flex flex-row">
+      <v-btn
+        class="buttons mx-2"
+        text
+        v-for="menu in menus"
+        :key="`menu-${menu.title}`"
+        v-scroll-to="`#${menu.refElement}`"
+      >
+        <span>{{ menu.title }}</span>
+      </v-btn>
+    </div>
+
+    <v-menu class="d-flex d-sm-none" bottom left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn class="d-flex d-sm-none" dark icon v-bind="attrs" v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="menu in menus"
+          :key="`menu-${menu.title}-responsive`"
+        >
+          <v-btn
+            v-scroll-to="`#${menu.refElement}`"
+            class="buttons"
+            width="100%"
+            text
+          >
+            <span>{{ menu.title }}</span>
+          </v-btn>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      menus: [
+        { title: "INÍCIO", refElement: "section-inicio" },
+        { title: "SERVIÇOS", refElement: "section-servicos" },
+        { title: "PORTFÓLIO", refElement: "section-portfolio" },
+        { title: "CONTATO", refElement: "section-contato" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.buttons:hover {
+  background-color: white;
+  color: #4bcaff;
+}
+</style>
