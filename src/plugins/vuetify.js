67b50fabb06c28bac52590f9bf9294d4e1afc84c
Matheus Eduardo França <matheusefranca1727@gmail.com>
2022-03-26 22:06:27 -0300

    Implementando vuetify e scrollTo na instancia vue

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,15 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        greyBackground: "#4B565D",
+        blueBackground: '#4bcaff'
+      },
+    },
+  },
 });
