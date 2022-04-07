1e9db86fc843fe0c9a91eed929ab33defc7cec59
Matheus Eduardo França <matheusefranca1727@gmail.com>
2022-04-07 09:11:28 -0300

    Implementando component Section como padrão do site

---
+++ src/components/Section.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-main :id="idSection" :class="background + ' pa-6 d-flex flex-column'">
+    <div class="mb-10">
+      <h2 :class="`${textColor}--text` + ' font-weight-light text-h4 mb-1'">
+        {{ title | textUpperCase() }}
+      </h2>
+      <v-divider :class="textColor + ' divider'"></v-divider>
+    </div>
+    <section
+      class="d-flex flex-column flex-md-row align-start justify-space-around"
+    >
+      <slot></slot>
+    </section>
+  </v-main>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+      require: true,
+    },
+    idSection: {
+      type: String,
+      default: "",
+      require: true,
+    },
+    background: {
+      type: String,
+      default: "white",
+      require: false,
+    },
+    textColor: {
+      type: String,
+      default: "white",
+      require: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.divider {
+  width: 8%;
+  padding: 0.05em 0;
+}
+</style>
