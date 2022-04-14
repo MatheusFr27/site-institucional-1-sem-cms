50a73c26a6079303e11606c27a0c5bc4ebd23b9c
Matheus França <matheus.franca@sociisrh.com.br>
2022-04-13 23:25:09 -0300

    Criando componente de formulário de contrato

---
+++ src/components/FormContact.vue
@@ -0,0 +1,188 @@
+<template>
+  <form ref="formEmail" @submit.prevent="sendEmail()">
+    <div class="d-flex flex-column my-4">
+      <label class="white--text font-weight-semibold" for="user_name">{{
+        "Nome" | textUpperCase()
+      }}</label>
+      <v-text-field
+        solo
+        dense
+        single-line
+        hide-details
+        id="user_name"
+        name="user_name"
+        ref="user_name"
+        v-model.trim="form.user_name"
+      ></v-text-field>
+    </div>
+    <div class="d-flex flex-column my-4">
+      <label class="white--text font-weight-semibold" for="user_email"
+        >{{ "email" | textUpperCase() }} <span class="red--text">*</span></label
+      >
+      <v-text-field
+        solo
+        dense
+        single-line
+        hide-details
+        type="email"
+        id="user_email"
+        name="user_email"
+        ref="user_email"
+        v-model.trim="form.user_email"
+      ></v-text-field>
+    </div>
+    <div class="d-flex flex-column my-4">
+      <label class="white--text font-weight-semibold" for="message"
+        >{{ "Mensagem" | textUpperCase() }}
+        <span class="red--text">*</span></label
+      >
+      <v-textarea
+        solo
+        dense
+        single-line
+        hide-details
+        type="text"
+        id="message"
+        name="message"
+        ref="message"
+        v-model.trim="form.message"
+      ></v-textarea>
+
+      <v-btn
+        class="my-8"
+        dark
+        :rounded="false"
+        :loading="loadingButtonContact"
+        :disabled="loadingButtonContact"
+        color="info"
+        type="submit"
+      >
+        Enviar
+        <template v-slot:loader>
+          <span class="custom-loader">
+            <v-icon color="white" dark>mdi-cached</v-icon>
+          </span>
+        </template>
+      </v-btn>
+    </div>
+    <Alert
+      :text="alert.text"
+      :typeAlert="alert.type"
+      :show="alert.show"
+      @invisible="alert.show = false"
+    />
+  </form>
+</template>
+
+<script>
+import emailjs from "@emailjs/browser";
+import Alert from "./Alert.vue";
+
+export default {
+  name: "FormContact",
+  components: {
+    Alert,
+  },
+  data() {
+    return {
+      loadingButtonContact: false,
+      form: {
+        user_name: "",
+        user_email: "",
+        message: "",
+      },
+      alert: {
+        text: "",
+        type: "info",
+        show: false,
+      },
+    };
+  },
+  methods: {
+    async sendEmail() {
+      if (this.validateForm()) return;
+
+      this.loadingButtonContact = true;
+
+      let typeAlert = "";
+      let textAlert = "";
+      try {
+        const result = await emailjs.sendForm(
+          "service_le5kctf",
+          "template_a0u47hi",
+          this.$refs.formEmail,
+          "nDZCWfLZv49nMzOQR"
+        );
+
+        typeAlert = result.status === 200 ? "success" : "error";
+        textAlert =
+          result.status === 200
+            ? "E-mail enviado com sucesso."
+            : "Houve um erro ao enviar o e-mail, tente mais tarde.";
+      } catch (error) {
+        if (error.status === 400) {
+          typeAlert = "error";
+          textAlert = "Houve um erro ao enviar o e-mail, tente mais tarde.";
+        }
+      }
+
+      this.alert.text = textAlert;
+      this.alert.type = typeAlert;
+      this.alert.show = true;
+
+      this.loadingButtonContact = false;
+
+      this.form.user_name = "";
+      this.form.user_email = "";
+      this.form.message = "";
+    },
+    validateForm() {
+      if (!this.form.user_email || !this.form.message) {
+        return true;
+      }
+
+      return false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* css do botão de contatos */
+.custom-loader {
+  animation: loader 1s infinite;
+  display: flex;
+}
+@-moz-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
